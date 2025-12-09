27e75f456e6eac5cb293741c872274a8afa5149e
spaceeye <superspaceeye@gmail.com>
2025-12-09 21:13:41 +0300

    fix

--- common/src/main/java/net/spaceeye/vmod/mixin/ShipObjectServerWorldMixin.java
+++ common/src/main/java/net/spaceeye/vmod/mixin/ShipObjectServerWorldMixin.java
@@ -3,11 +3,11 @@ package net.spaceeye.vmod.mixin;
 import net.spaceeye.vmod.events.AVSEvents;
 import net.spaceeye.vmod.vsStuff.CustomBlockMassManager;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 import org.valkyrienskies.core.api.ships.ServerShip;
 import org.valkyrienskies.core.impl.shadow.Er;
 import org.valkyrienskies.core.internal.world.chunks.VsiBlockType;
@@ -22,43 +22,25 @@ abstract public class ShipObjectServerWorldMixin {
         AVSEvents.INSTANCE.getServerShipRemoveEvent().emit(new AVSEvents.ServerShipRemoveEvent(ship));
     }
 
-    @Unique int vmod$posX;
-    @Unique int vmod$posY;
-    @Unique int vmod$posZ;
-    @Unique String vmod$dimensionId;
-    @Unique VsiBlockType vmod$oldBlockType;
-    @Unique VsiBlockType vmod$newBlockType;
-    @Unique double vmod$oldBlockMass;
-    @Unique double vmod$newBlockMass;
-
-    @Inject(
+    @ModifyArgs(
             method = "onSetBlock",
-            at = @At(value = "HEAD"),
+            at = @At(value = "INVOKE", target = "Lorg/valkyrienskies/core/impl/shadow/Et;onSetBlock(IIILjava/lang/String;Lorg/valkyrienskies/core/internal/world/chunks/VsiBlockType;Lorg/valkyrienskies/core/internal/world/chunks/VsiBlockType;DD)V"),
             remap = false
     )
-    void vmod$captureArgs(int posX, int posY, int posZ, String dimensionId, VsiBlockType oldBlockType, VsiBlockType newBlockType, double oldBlockMass, double newBlockMass, CallbackInfo ci) {
-        vmod$posX = posX;
-        vmod$posY = posY;
-        vmod$posZ = posZ;
-        vmod$dimensionId = dimensionId;
-        vmod$oldBlockType = oldBlockType;
-        vmod$newBlockType = newBlockType;
-        vmod$oldBlockMass = oldBlockMass;
-        vmod$newBlockMass = newBlockMass;
-    }
+    void vmod$onSetBlock(Args args) {
+        var posX = (int)args.get(0);
+        var posY = (int)args.get(1);
+        var posZ = (int)args.get(2);
+        var dimensionId = (String)args.get(3);
+        var oldBT = (VsiBlockType)args.get(4);
+        var newBT = (VsiBlockType)args.get(5);
+        var oldMass = (double)args.get(6);
 
-    @ModifyArg(
-            method = "onSetBlock",
-            at = @At(value = "HEAD"),
-            remap = false,
-            index = 6
-    )
-    double vmod$onSetBlock(double oldBlockMass) {
-        Double mass = CustomBlockMassManager.INSTANCE.getCustomMass(vmod$dimensionId, vmod$posX, vmod$posY, vmod$posZ);
-        if (vmod$oldBlockType != vmod$newBlockType) {
-            CustomBlockMassManager.INSTANCE.removeCustomMass(vmod$dimensionId, vmod$posX, vmod$posY, vmod$posZ);
+        Double mass = CustomBlockMassManager.INSTANCE.getCustomMass(dimensionId, posX, posY, posZ);
+        if (oldBT != newBT) {
+            CustomBlockMassManager.INSTANCE.removeCustomMass(dimensionId, posX, posY, posZ);
         }
 
-        return Objects.requireNonNullElse(mass, oldBlockMass);
+        args.set(6, Objects.requireNonNullElse(mass, oldMass));
     }
 }
